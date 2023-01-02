252c077dd1fb4e01c0beca3e36d5fcdf09417aae
Tolotra Rabefaly <tolotor@yahoo.fr>
2023-01-02 14:01:56 +0300

    Js update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import { setNavbarCollapses } from "./components/navbarCollpase.js";
+
+setNavbarCollapses({
+	onToggle(open) {
+		if (open) {
+			document.body.style.overflowY = "hidden";
+			window.scrollTo({ top: 0 });
+		} else {
+			document.body.style.overflowY = "auto";
+		}
+	},
+});
